dd2732181084c794147c45c43dcfe5f2ada317b0
mgaldino <mcz.fea@gmail.com>
2022-10-28 16:12:16 -0300

    Importando arquivos do 1t

---
+++ import_files.R
@@ -0,0 +1,47 @@
+install.packages("googledrive")
+install.packages("googlesheets4", dependencies=T)
+# caregando bibliotecas
+
+library(googlesheets4)
+library(googledrive)
+library(httpuv)
+library(tidyverse)
+library(purrr)
+library(data.table)
+library(janitor)
+library(stringr)
+
+
+## store the URL you have
+folder_url <- "https://drive.google.com/drive/folders/1RlSRfMqk2tPiSPDqhad5MmNWR9DLmm8T"
+
+## identify this folder on Drive
+## let googledrive know this is a file ID or URL, as opposed to file name
+folder <- drive_get(as_id(folder_url))
+
+## identify the csv files in that folder
+csv_files <- drive_ls(folder, type = "csv")
+
+# download the files
+walk(csv_files$id, ~ drive_download(as_id(.x)))
+
+setwd( "/home/mgaldino/Downloads")
+# 
+lista_arquivos <- list.files()
+n <- length(lista_arquivos)
+lista_df <- list()
+
+for(i in 1:n){
+  df <- fread(lista_arquivos[i], encoding = "Latin-1")
+  
+  df <- df %>%
+    mutate(id = i,
+           hora <- substr(lista_arquivos[i], 26, 39)) %>%
+  clean_names() %>%
+    filter(secoes_totalizadas > 0)
+  
+  lista_df[[i]] <- df
+  print(i)
+}
+df_voto1t <- lista_df %>% bind_rows()
+
